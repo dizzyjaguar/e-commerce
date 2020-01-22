5de7525e2ec2ef6e8e63da30625df08af64886e5
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-21 17:51:45 -0800

    create product data and export as default

---
+++ data/morning-glories.js
@@ -0,0 +1,62 @@
+const heavenlyBlue = {
+    id: 'heavenly blue',
+    name: 'Heavenly Blue',
+    image: '',
+    genera: 'Ipomoea Purpurea',
+    color: 'blue',
+    price: 3.00,
+};
+
+const knowliansBlack = {
+    id: 'knowlians black',
+    name: 'Knowlians Black',
+    image: '',
+    genera: 'Ipomoea Purpurea',
+    color: 'dark-red, black',
+    price: 3.50,
+};
+
+const japaneseRuffled = {
+    id: 'japanese ruffled',
+    name: 'Japanese Ruffled',
+    image: '',
+    genera: 'Ipomoea Nil',
+    color: 'bright-red, white',
+    price: 4.25,
+};
+
+const bluePicotee = {
+    id: 'blue picotee',
+    name: 'Blue Picotee',
+    image: '',
+    genera: 'Ipomoea Nil',
+    color: 'purple',
+    price: 5.50,
+};
+
+const redPicotee = {
+    id: 'red picotee',
+    name: 'Red Picotee',
+    image: '',
+    genera: 'Ipomoea Nil',
+    color: 'bright-red, white',
+    price: ,
+};
+
+const flyingSaucers = {
+    id: 'flying saucers',
+    name: 'Flying Saucers',
+    image: '',
+    genera: 'Ipomoea Purpurea',
+    color: 'blue, white',
+    price: 3.00,
+};
+
+const scarlettOhara = {
+    id: 'scarlett ohara',
+    name: 'Scarlett Ohara',
+    image: '',
+    genera: 'Ipomoea Bil',
+    color: 'red',
+    price: 3.00,
+};
