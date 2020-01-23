3ae0bd3e8e8e6a0f8e93a0d7973df11524838e4c
Eli Zevin <dizzyjaguar@gmail.com>
2020-01-22 17:39:07 -0800

    create findById function

---
+++ common/utils.js
@@ -0,0 +1,8 @@
+export const findById = (someId, someArray) => {
+    for (let i = 0; i < someArray.length; i++){
+        const item = someArray[i];
+        if (item.id === someId){
+            return dispatchEvent;
+        }
+    }
+};
